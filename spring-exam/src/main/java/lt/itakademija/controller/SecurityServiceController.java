5a6e33279bba9dca03ee7b4493fa6d99e710577d
Osvaldas <o.rimkus@gmail.com>
2016-12-21 00:08:37 +0200

    Spring 3 and 4 tests still failing loosing hope

--- spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
+++ spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
@@ -3,8 +3,10 @@ package lt.itakademija.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -17,7 +19,7 @@ import lt.itakademija.repository.SecurityEventsRepository;
 
 @RestController
 @Api(value = "Security controller")
-@RequestMapping("/webapi/events") // http://localhost:9092/spring-exam
+@RequestMapping("http://localhost:9092/spring-exam/webapi/events")
 public class SecurityServiceController {
 
 	@Autowired
@@ -28,6 +30,7 @@ public class SecurityServiceController {
     }
     
     @RequestMapping(method = RequestMethod.GET)// 3rd task's addition
+    @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "Get all registered events", notes = "Returns a list of all rgistered events with their information.")
     public List<RegisteredEvent> getRegisteredEvents() {
         return repository.getEvents();
@@ -35,6 +38,7 @@ public class SecurityServiceController {
     }
     
     @RequestMapping(method = RequestMethod.POST)// 3rd task's addition
+    @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Adds a new event", notes = "Adds a new event to the current event list.")
     public RegisteredEvent createEvent(@ApiParam(value = "Data of new event", required = true) EventRegistration registrationData) {
     	return repository.create(registrationData);
@@ -42,6 +46,7 @@ public class SecurityServiceController {
     }
     
     @RequestMapping(method = RequestMethod.DELETE)// 3rd task's addition
+    @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "Deletes a specific event", notes = "Deletes an avent from database based on the supplied event id.")
     public RegisteredEvent deleteEvent(@ApiParam(value = "Event id", required = true) Long id) {
         return repository.delete(id);
@@ -49,6 +54,7 @@ public class SecurityServiceController {
     }
 
     @RequestMapping(method = RequestMethod.PUT)// 3rd task's addition
+    @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "Updates an event", notes = "Updates and event that already exists in the database.")
     public RegisteredEvent updateEvent(@ApiParam(value = "Event id", required = true) Long id, @ApiParam(value = "Data of event to bu updated", required = true) RegisteredEventUpdate updateData) {
         return repository.update(id, updateData);
