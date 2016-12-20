16f9eb01245aaddecffdc046586c85d0f885ab8f
Osvaldas <o.rimkus@gmail.com>
2016-12-20 23:16:25 +0200

    Spring 3 and 4 tests still failing

--- spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
+++ spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -16,7 +17,7 @@ import lt.itakademija.repository.SecurityEventsRepository;
 
 @RestController
 @Api(value = "Security controller")
-@RequestMapping("/spring-exam/webapi/events")
+@RequestMapping("/webapi/events") // http://localhost:9092/spring-exam
 public class SecurityServiceController {
 
 	@Autowired
@@ -26,28 +27,28 @@ public class SecurityServiceController {
         this.repository = repository;
     }
     
-    //@RequestMapping(method = RequestMethod.GET)// 3rd task's addition
+    @RequestMapping(method = RequestMethod.GET)// 3rd task's addition
     @ApiOperation(value = "Get all registered events", notes = "Returns a list of all rgistered events with their information.")
     public List<RegisteredEvent> getRegisteredEvents() {
         return repository.getEvents();
     	//throw new UnsupportedOperationException("not implemented");
     }
     
-    //@RequestMapping(method = RequestMethod.POST)// 3rd task's addition
+    @RequestMapping(method = RequestMethod.POST)// 3rd task's addition
     @ApiOperation(value = "Adds a new event", notes = "Adds a new event to the current event list.")
     public RegisteredEvent createEvent(@ApiParam(value = "Data of new event", required = true) EventRegistration registrationData) {
     	return repository.create(registrationData);
     	//throw new UnsupportedOperationException("not implemented");
     }
     
-    //@RequestMapping(method = RequestMethod.DELETE)// 3rd task's addition
+    @RequestMapping(method = RequestMethod.DELETE)// 3rd task's addition
     @ApiOperation(value = "Deletes a specific event", notes = "Deletes an avent from database based on the supplied event id.")
     public RegisteredEvent deleteEvent(@ApiParam(value = "Event id", required = true) Long id) {
         return repository.delete(id);
         //throw new UnsupportedOperationException("not implemented");
     }
 
-    //@RequestMapping(method = RequestMethod.POST)// 3rd task's addition
+    @RequestMapping(method = RequestMethod.PUT)// 3rd task's addition
     @ApiOperation(value = "Updates an event", notes = "Updates and event that already exists in the database.")
     public RegisteredEvent updateEvent(@ApiParam(value = "Event id", required = true) Long id, @ApiParam(value = "Data of event to bu updated", required = true) RegisteredEventUpdate updateData) {
         return repository.update(id, updateData);
