baf92c890da968c2fee352444eb5ca067e7a9ec8
Domas Karklys <domas.karklys@gmail.com>
2016-12-20 15:17:04 +0200

    Fixed @Component annotation

--- spring-exam/src/main/java/lt/itakademija/repository/SimpleDateProvider.java
+++ spring-exam/src/main/java/lt/itakademija/repository/SimpleDateProvider.java
@@ -3,9 +3,12 @@ package lt.itakademija.repository;
 import java.time.Instant;
 import java.util.Date;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by mariusg on 2016.12.19.
  */
+@Component
 public class SimpleDateProvider implements DateProvider {
 
     @Override
--- spring-exam/src/main/java/lt/itakademija/repository/SimpleSequenceNumberGenerator.java
+++ spring-exam/src/main/java/lt/itakademija/repository/SimpleSequenceNumberGenerator.java
@@ -1,8 +1,11 @@
 package lt.itakademija.repository;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by mariusg on 2016.12.19.
  */
+@Component
 public final class SimpleSequenceNumberGenerator implements SequenceNumberGenerator {
     
     private Long number = 1L;
