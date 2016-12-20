969b9e0d56dfd73c26906b94efdc2bbafb708f6f
Domas Karklys <domas.karklys@gmail.com>
2016-12-20 15:20:21 +0200

    Implemented Event controller methods and added annotations

--- spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
+++ spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
@@ -1,34 +1,61 @@
 package lt.itakademija.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import lt.itakademija.model.EventRegistration;
 import lt.itakademija.model.RegisteredEvent;
 import lt.itakademija.model.RegisteredEventUpdate;
 import lt.itakademija.repository.SecurityEventsRepository;
 
-import java.util.List;
-
+@RestController
 public class SecurityServiceController {
 
     private final SecurityEventsRepository repository;
 
+    @Autowired
     public SecurityServiceController(final SecurityEventsRepository repository) {
         this.repository = repository;
     }
 
+    @GetMapping("webapi/events/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RegisteredEvent getRegisteredEvent(@PathVariable Long id) {
+        return repository.getEvent(id);
+    }
+    
+    @GetMapping("webapi/events")
+    @ResponseStatus(HttpStatus.OK)
     public List<RegisteredEvent> getRegisteredEvents() {
-        throw new UnsupportedOperationException("not implemented");
+        return repository.getEvents();
     }
 
-    public RegisteredEvent createEvent(EventRegistration registrationData) {
-        throw new UnsupportedOperationException("not implemented");
+    @PostMapping("webapi/events")
+    @ResponseStatus(HttpStatus.CREATED)
+    public RegisteredEvent createEvent(@RequestBody EventRegistration registrationData) {
+        return repository.create(registrationData);
     }
 
-    public RegisteredEvent deleteEvent(Long id) {
-        throw new UnsupportedOperationException("not implemented");
+    @DeleteMapping("webapi/events/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RegisteredEvent deleteEvent(@PathVariable Long id) {
+        return repository.delete(id);
     }
 
-    public RegisteredEvent updateEvent(Long id, RegisteredEventUpdate updateData) {
-        throw new UnsupportedOperationException("not implemented");
+    @PutMapping("webapi/events/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RegisteredEvent updateEvent(@PathVariable Long id, RegisteredEventUpdate updateData) {
+        return repository.update(id, updateData);
     }
 
 }
