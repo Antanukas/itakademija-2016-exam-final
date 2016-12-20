16ba27d4e9d34e9d0bed67d4a996c88c36709fa6
Osvaldas <o.rimkus@gmail.com>
2016-12-20 17:57:15 +0200

    Spring first exercise done

--- spring-exam/src/main/java/lt/itakademija/repository/InMemorySecurityEventsRepository.java
+++ spring-exam/src/main/java/lt/itakademija/repository/InMemorySecurityEventsRepository.java
@@ -3,7 +3,10 @@ package lt.itakademija.repository;
 import lt.itakademija.model.EventRegistration;
 import lt.itakademija.model.RegisteredEvent;
 import lt.itakademija.model.RegisteredEventUpdate;
+import lt.itakademija.model.SeverityLevel;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -16,6 +19,9 @@ public final class InMemorySecurityEventsRepository implements SecurityEventsRep
     private final SequenceNumberGenerator sequenceGenerator;
 
     private final DateProvider dateProvider;
+    
+    // MANO LISTAS
+    List<RegisteredEvent> eventList = new ArrayList<RegisteredEvent>();
 
     public InMemorySecurityEventsRepository(SequenceNumberGenerator sequenceGenerator, DateProvider dateProvider) {
         this.sequenceGenerator = sequenceGenerator;
@@ -30,22 +36,57 @@ public final class InMemorySecurityEventsRepository implements SecurityEventsRep
      */
     @Override
     public RegisteredEvent create(EventRegistration eventRegistration) {
-        throw new UnsupportedOperationException("not implemented");
+        Long newId = sequenceGenerator.getNext();
+        Date newDate = dateProvider.getCurrentDate();
+        SeverityLevel newSeverityLevel = eventRegistration.getSeverityLevel();
+        String newLocation = eventRegistration.getLocation();
+        String newDescription = eventRegistration.getDescription();
+        
+    	RegisteredEvent newEvent = new RegisteredEvent(newId, newDate, newSeverityLevel, newLocation, newDescription);
+    	eventList.add(newEvent);
+    	return newEvent;
+    	//throw new UnsupportedOperationException("not implemented");
     }
 
     @Override
     public List<RegisteredEvent> getEvents() {
-        throw new UnsupportedOperationException("not implemented");
+        return eventList;
+    	//throw new UnsupportedOperationException("not implemented");
     }
 
     @Override
     public RegisteredEvent delete(Long id) {
-        throw new UnsupportedOperationException("not implemented");
+    	RegisteredEvent objectToRemove = null;
+    	for(RegisteredEvent event : eventList){
+        	if (event.getId() == id){
+        		objectToRemove = event;
+        		break;
+        	}
+        }
+    	eventList.remove(objectToRemove);
+    	return objectToRemove;
+    	//throw new UnsupportedOperationException("not implemented");
     }
 
     @Override
     public RegisteredEvent update(Long id, RegisteredEventUpdate registeredEventUpdate) {
-        throw new UnsupportedOperationException("not implemented");
+    	RegisteredEvent eventToEdit = null;
+    	for(RegisteredEvent event : eventList){
+        	if (event.getId() == id){
+        		eventToEdit = event;
+        		eventList.remove(event);
+        		break;
+        	}
+        }
+        Long newId = eventToEdit.getId();
+    	Date newDate = eventToEdit.getRegistrationDate();
+    	SeverityLevel newSeverityLevel = registeredEventUpdate.getSeverityLevel();
+        String newLocation = eventToEdit.getLocation();
+        String newDescription = eventToEdit.getDescription();
+        RegisteredEvent newEvent = new RegisteredEvent(newId, newDate, newSeverityLevel, newLocation, newDescription);
+        eventList.add(newEvent);
+        return newEvent;
+        //throw new UnsupportedOperationException("not implemented");
     }
 
 }
