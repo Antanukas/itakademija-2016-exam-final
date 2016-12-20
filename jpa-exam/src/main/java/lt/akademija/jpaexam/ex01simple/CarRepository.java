3c0dfb3f4e90d7e9564c1e119268be998170957f
Domas Karklys <domas.karklys@gmail.com>
2016-12-20 11:33:09 +0200

    Solved ex01 - annotated CarEntity and implemented CarRepository methods

--- jpa-exam/src/main/java/lt/akademija/jpaexam/ex01simple/CarRepository.java
+++ jpa-exam/src/main/java/lt/akademija/jpaexam/ex01simple/CarRepository.java
@@ -2,18 +2,29 @@ package lt.akademija.jpaexam.ex01simple;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class CarRepository {
 
+    @Autowired
+    private EntityManager em;
+    
     /**
      * Searches database for all cars and returns list of them
      * @return
      */
 
     public List<CarEntity> findAll() {
-        throw new UnsupportedOperationException();
+        String queryTxt = "SELECT c FROM CarEntity c";
+        Query q = em.createQuery(queryTxt, CarEntity.class);
+        return q.getResultList();
     }
 
     /**
@@ -21,14 +32,21 @@ public class CarRepository {
      * If car is not present `null` is returned.
      */
     public CarEntity find(Long id) {
-        throw new UnsupportedOperationException();
+        return em.find(CarEntity.class, id);
     }
 
     /**
      * Saves or updates car information. When car with existing ID is passed then update operation is performed.
      * When id is not present new car is saved to database
      */
-    public CarEntity saveOrUpdate(CarEntity e) {
-        throw new UnsupportedOperationException();
+    
+    @Transactional
+    public CarEntity saveOrUpdate(CarEntity car) {
+        if (car.getId() != null && find(car.getId()) != null) {
+            return em.merge(car);
+        } else {
+            em.persist(car);
+            return car;
+        }
     }
 }
