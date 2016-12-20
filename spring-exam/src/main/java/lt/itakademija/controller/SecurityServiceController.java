dd5a6f9556284d7598bb059a45d84d54b9019716
andrius444 <andrius.sareika@gmail.com>
2016-12-20 23:28:34 +0200

    Javadocs generated

--- spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
+++ spring-exam/src/main/java/lt/itakademija/controller/SecurityServiceController.java
@@ -8,9 +8,15 @@ import lt.itakademija.repository.SecurityEventsRepository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "webapi/events")
 public class SecurityServiceController {
 
 	@Autowired
@@ -20,21 +26,25 @@ public class SecurityServiceController {
         this.repository = repository;
     }
 
+	@GetMapping
     public List<RegisteredEvent> getRegisteredEvents() {
         return repository.getEvents();
     	//throw new UnsupportedOperationException("not implemented");
     }
 
+	@PostMapping
     public RegisteredEvent createEvent(EventRegistration registrationData) {
         return repository.create(registrationData);
     	//throw new UnsupportedOperationException("not implemented");
     }
 
+	@DeleteMapping
     public RegisteredEvent deleteEvent(Long id) {
         return repository.delete(id);
     	//throw new UnsupportedOperationException("not implemented");
     }
 
+    @PutMapping
     public RegisteredEvent updateEvent(Long id, RegisteredEventUpdate updateData) {
         return repository.update(id, updateData);
     	//throw new UnsupportedOperationException("not implemented");
